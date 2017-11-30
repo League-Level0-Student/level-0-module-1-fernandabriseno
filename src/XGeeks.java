69af2ab9f88100827413701c8a1e00ad709e92ec
Briseno.Fernanda <Briseno.Fernanda@SEMS-259-12.SEMS259.LAB>
2017-11-29 16:26:22 -0800

    XGeeks

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -11,14 +11,22 @@ import javax.swing.JOptionPane;
 * the superpowers of all the people in the classroom. E.g. When I type "June",
 * your program should say "June's superpower is writing recipes".
 */
-public class XGeeks {
+public class XGeeks { 	
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+	String MattsPower = "talking to cats"; 
+	String TiernansPower = "Flying"; 
+	String FernandasPower = "turn into a dabasaurus-rex";
+	String AndrewsPower = "Turn into an asian panda"; 
+	String RyansPower = "Mind controlling";
+	String NathanPower = "hacking free roblox and the pentagon"; 
+	String EnterNamePower = "person that has nothing better to do than do this... it is my great honor... I have no honor... to grant you the power of..... reading this!!!!!! :) GOODBYE!";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String a=JOptionPane.showInputDialog("Enter name");
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
+JOptionPane.showMessageDialog(null,"Hello..."+ EnterNamePower); 
+if(a.equals("EnterNamePower"));
 
 	}
 }
